168c75cabad327f40aec021f762b75c87cd4a29e
Luca Sasselli <4eversax@gmail.com>
2017-04-30 00:37:14 +0200

    Removed secrets

--- mobile/src/main/java/com/lucasasselli/evernotewear/Constants.java
+++ mobile/src/main/java/com/lucasasselli/evernotewear/Constants.java
@@ -6,8 +6,8 @@ public class Constants {
     // Change this from SANDBOX to PRODUCTION for production
     public static final EvernoteSession.EvernoteService EVERNOTE_SERVICE = EvernoteSession.EvernoteService.SANDBOX;
 
-    public static final String EVERNOTE_KEY = "4eversax-3979";
-    public static final String EVERNOTE_SECRET = "206f5fe1a25632ad";
+    public static final String EVERNOTE_KEY = "";
+    public static final String EVERNOTE_SECRET = "";
 
     public static final String PREFERENCES = "evernote_wear_preferences";
 }
